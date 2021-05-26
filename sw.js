936a61a6159b5915641294dc36588ee43494ed47
fhs42724 <fhs42724@fh-salzburg.ac.at>
2021-05-26 11:46:25 +0200

    Update NotificationMessage

--- sw.js
+++ sw.js
@@ -97,8 +97,9 @@ self.addEventListener("push", function (event) {
   console.log("[Service Worker] Push Received.");
   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-  const title = "Push Codelab";
+  const title = "BigBrain Zitat";
   const options = {
+    icon: '/icon192.png',
     body: `${event.data.text()}`,
   };
 
